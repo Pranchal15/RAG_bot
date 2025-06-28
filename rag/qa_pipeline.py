9c1890c568d104b5ffc9ca853eccf636f2691f3a
Pranchal Agarwal <pranchalagar_umass_edu@uri-gpu002.unity.rc.umass.edu>
2025-06-28 21:41:35 +0000

    switched to mistralv0.3

--- rag/qa_pipeline.py
+++ rag/qa_pipeline.py
@@ -7,7 +7,7 @@ def get_qa_chain(vectorstore):
     llm = ChatOpenAI(
         openai_api_key=os.getenv("OPENAI_API_KEY"),
         openai_api_base="https://api.together.xyz/v1",
-        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
+        model="mistralai/Mixtral-8x7B-Instruct-v0.3",
         temperature=0.7,  # Controls randomness            
         max_tokens=1024
     )
