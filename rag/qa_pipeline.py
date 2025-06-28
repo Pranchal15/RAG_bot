9cf324fb7b25a007d9f33f127a613600bcf10686
Pranchal Agarwal <pranchalagar_umass_edu@gpu022.unity.rc.umass.edu>
2025-06-28 01:14:05 +0000

    added openai

--- rag/qa_pipeline.py
+++ rag/qa_pipeline.py
@@ -5,7 +5,7 @@ from langchain.chat_models import ChatOpenAI
 
 def get_qa_chain(vectorstore):
     llm = ChatOpenAI(
-        openai_api_key=os.getenv("TOGETHER_API_KEY"),
+        openai_api_key=os.getenv("OPENAI_API_KEY"),
         openai_api_base="https://api.together.xyz/v1",
         model="mistralai/Mixtral-8x7B-Instruct-v0.1"
     )
